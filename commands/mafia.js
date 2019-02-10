cdb545fc15a38829ae28118722d8fd53d260141c
WrathOfAthens <austinaaronabel@gmail.com>
2019-02-10 00:51:16 -0600

    Starting dev branch with mafia.js

---
+++ commands/mafia.js
@@ -0,0 +1,13 @@
+/**
+ * @file The index point of Ashe
+ * @author WrathOfAthens
+ * @license GNU-GPLv3
+ */
+
+module.exports = {
+	name: 'mafia',
+    description: 'A game of Mafia with players in the server',
+	execute(msg, args) {
+        
+	},
+};
