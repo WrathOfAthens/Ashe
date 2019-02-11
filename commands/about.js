031506365c749c9a2808cee0e1710bc2b9feb760
WrathOfAthens <austinaaronabel@gmail.com>
2019-02-10 18:41:05 -0600

    Fixed bug on about.js

--- commands/about.js
+++ commands/about.js
@@ -3,12 +3,13 @@
  * @author WrathOfAthens
  * @license GNU-GPLv3
  */
+const { version } = require('./package.json');
 
 module.exports = {
 	name: 'about',
     description: 'Prints information about Ashe',
 	execute(msg, args) {
-		msg.author.send(`=================\nAshe(${pkg.version}) by WrathOfAthens\nLogged in as ` +
+		msg.author.send(`=================\nAshe(${version}) by WrathOfAthens\nLogged in as ` +
         `${bot.user.tag} (${bot.user.id}) on ${bot.guilds.size} server(s)\nMeow!\n=================\n`)
 	},
 };
