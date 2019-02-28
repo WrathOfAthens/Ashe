527a66e38e838c7588fcc2170e5b196ca29c12af
WrathOfAthens <austinaaronabel@gmail.com>
2019-02-28 14:04:29 -0600

    Added beta purge command

---
+++ commands/purge.js
@@ -0,0 +1,33 @@
+/**
+ * @file Purges 'x' number of messages
+ * @author WrathOfAthens
+ * @license GNU-GPLv3
+ */
+const Keyv = require('keyv');
+const keyv = new Keyv(`${process.env.mongodb}`);
+
+module.exports = {
+	name: 'purge',
+    description: 'Deletes 2-100 messages [MUST BE ENABLED]',
+    args: '[number of messages] OR [enable] OR [disable]',
+	execute(msg, args) {
+        if (!args[0]) return msg.channel.send(`Expecting arguments`);
+        
+        if (args[0] === 'enable') {
+            await keyv.set(`PURGE${msg.guild.id}`, true);
+            return msg.channel.send(`Purge is now enabled.`);
+        } else if (args[0] === 'disable') {
+            await keyv.set(`PURGE${msg.guild.id}`, false);
+            return msg.channel.send(`Purge is now disabled.`);
+        }
+
+        const purgeEnabled = await keyv.get(`PURGE${msg.guild.id}`);
+
+        if (purgeEnabled === true) {
+            return msg.channel.send(`Purge is enabled.`);
+        } else {
+            return msg.channel.send(`Purge is disabled.`);
+        }
+
+	},
+};
